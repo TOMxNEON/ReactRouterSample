e6b44c78e52f3121631b017d02d57a4d954660a7
Tom Xavier <tomxavieronthego@gmail.com>
2023-07-22 10:34:08 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import About from "./components/About";
+import Profile from "./components/Profile";
+import Blog from "./components/Blog";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [state, setState] = useState('');
+    let component 
+    if(state==='about' ){
+      component=<About></About>
+    }
+    if(state==='profile'){
+      component=<Profile></Profile>
+    }
+    if(state==='blog'){
+      component=<Blog></Blog>
+    }
+   
+
+    return (
+        <div className="App">
+            <button onClick={() => setState('about')}>About</button>
+            <button onClick={() => setState('profile')}>Profile</button>
+            <button onClick={() => setState('blog')}>Blog</button>
+            {component}
+        </div>
+    );
 }
 
 export default App;
